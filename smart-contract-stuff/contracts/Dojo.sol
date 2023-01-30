ccaeb79144f2632c2c439bae73450cd9c2fc3f51
stackup-dev <james.limjy@gmail.com>
2023-01-30 21:47:30 +0800

    natspec + finished sc

--- smart-contract-stuff/contracts/Dojo.sol
+++ smart-contract-stuff/contracts/Dojo.sol
@@ -4,17 +4,38 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+/**
+ * @title Student teacher interaction faciliated with tokens
+ * @author jameslimjy
+ */
 contract Dojo is AccessControl, ERC20 {
     address private principal;
     uint256 public numTeachers;
     uint256 public numStudents;
     uint256 public nextClassId;
     uint256 public nextConsultId;
+    uint256 public nextAssignmentId;
     uint8 public constant STUDENT_STARTING_AMOUNT = 5;
 
     bytes32 public constant PRINCIPAL_ROLE = keccak256("PRINCIPAL_ROLE");
     bytes32 public constant TEACHER_ROLE = keccak256("TEACHER_ROLE");
     bytes32 public constant STUDENT_ROLE = keccak256("STUDENT_ROLE");
+
+    enum AssignmentApprovalState {
+        PENDING_APPROVAL,
+        APPROVED,
+        COMPLETED
+    }
+    enum ConsultAttendance {
+        NOT_SIGNED_UP,
+        SIGNED_UP,
+        ATTENDED
+    }
+    enum AssignmentParticipantState {
+        NOT_SIGNED_UP,
+        SIGNED_UP,
+        PAID_OUT
+    }
 
     struct Teacher {
         string name;
@@ -28,6 +49,7 @@ contract Dojo is AccessControl, ERC20 {
         address wallet;
         uint256[] classes;
         mapping(uint256 => ConsultAttendance) consults;
+        mapping(uint256 => AssignmentParticipantState) assignments;
     }
 
     struct Class {
@@ -38,11 +60,6 @@ contract Dojo is AccessControl, ERC20 {
         address[] students;
     }
 
-    struct ConsultAttendance {
-        bool signedUp;
-        bool attended;
-    }
-
     struct Consult {
         uint256 consultId;
         address teacher;
@@ -51,29 +68,49 @@ contract Dojo is AccessControl, ERC20 {
         address[] studentsSignedUp;
     }
 
+    struct Assignment {
+        uint256 assignmentId;
+        string description;
+        address teacher;
+        uint8 bounty;
+        address participant;
+        AssignmentApprovalState state;
+    }
+
     mapping(address => Teacher) public teachers;
     mapping(address => Student) public students;
     mapping(uint256 => Class) public classes;
     mapping(uint256 => Consult) public consults;
+    mapping(uint256 => Assignment) public assignments;
 
     event TeacherCreated(string indexed name, address indexed wallet);
     event StudentCreated(string indexed name, address indexed wallet);
     event ClassCreated(uint256 classId, string indexed className, uint8 cost, address indexed teacher);
     event ConsultCreated(uint256 consultId, address indexed teacher, uint8 stake, uint8 capacity);
+    event AssignmentCreated(uint256 assignmentId, address indexed teacher, uint8 bounty);
 
     constructor() ERC20("Dojo Token", "DOJO") {
         _setupRole(PRINCIPAL_ROLE, msg.sender);
         principal = msg.sender;
     }
 
-    function mint(uint256 amount) public onlyPrincipal {
+    /**
+     * @notice Mints tokens to the treasury
+     * @param amount The amount of tokens to mint
+     */
+    function mint(uint256 amount) external onlyPrincipal {
         _mint(address(this), amount);
     }
 
+    /**
+     * @notice For the principal to add a new teacher
+     * @param name The teacher's name
+     * @param wallet The teacher's wallet
+     */
     function addTeacher(
         string calldata name,
         address wallet
-    ) public onlyPrincipal isFreshWallet(wallet) notEmptyString(name) {
+    ) external onlyPrincipal isFreshWallet(wallet) notEmptyString(name) {
         _setupRole(TEACHER_ROLE, wallet);
         Teacher storage _teacher = teachers[wallet];
         _teacher.name = name;
@@ -82,10 +119,15 @@ contract Dojo is AccessControl, ERC20 {
         numTeachers++;
     }
 
+    /**
+     * @notice For the principal to add a new student
+     * @param name The student's name
+     * @param wallet The student's wallet
+     */
     function addStudent(
         string calldata name,
         address wallet
-    ) public onlyPrincipal isFreshWallet(wallet) notEmptyString(name) {
+    ) external onlyPrincipal isFreshWallet(wallet) notEmptyString(name) {
         require(balanceOf(address(this)) >= STUDENT_STARTING_AMOUNT, "Treasury has insufficient balance");
         _transfer(address(this), wallet, STUDENT_STARTING_AMOUNT);
         _setupRole(STUDENT_ROLE, wallet);
@@ -96,7 +138,12 @@ contract Dojo is AccessControl, ERC20 {
         numStudents++;
     }
 
-    function createClass(string calldata name, uint8 cost) public onlyTeacher notEmptyString(name) returns (uint256) {
+    /**
+     * @notice For a teacher to create a new class
+     * @param name The title of the class
+     * @param cost The amount of DOJO tokens it'll cost for a student to join the class
+     */
+    function createClass(string calldata name, uint8 cost) external onlyTeacher notEmptyString(name) {
         uint256 classId = nextClassId;
         Class storage _class = classes[classId];
         _class.name = name;
@@ -106,17 +153,25 @@ contract Dojo is AccessControl, ERC20 {
         _teacher.classes.push(classId);
         emit ClassCreated(classId, name, cost, msg.sender);
         nextClassId++;
-        return classId;
     }
 
-    function signUpForClass(uint256 classId) public onlyStudent classExists(classId) {
+    /**
+     * @notice For a student to sign up for a class
+     * @param classId The classID of the class the student wants to join
+     */
+    function signUpForClass(uint256 classId) external onlyStudent classExists(classId) {
         Class storage _class = classes[classId];
         _class.students.push(msg.sender);
         Student storage _student = students[msg.sender];
         _student.classes.push(classId);
     }
 
-    function createConsult(uint8 stake, uint8 capacity) public onlyTeacher returns (uint256) {
+    /**
+     * @notice For a teacher to create a consultation session
+     * @param stake The amount of DOJO tokens a student will have to put up as stake, will be returned after attendance marking
+     * @param capacity The maximum number of students allowed to join the consultation
+     */
+    function createConsult(uint8 stake, uint8 capacity) external onlyTeacher {
         uint256 consultId = nextConsultId;
         Consult storage _consult = consults[consultId];
         _consult.teacher = msg.sender;
@@ -126,20 +181,101 @@ contract Dojo is AccessControl, ERC20 {
         _teacher.consults.push(consultId);
         emit ConsultCreated(consultId, msg.sender, stake, capacity);
         nextConsultId++;
-        return consultId;
     }
 
-    function signUpForConsult(uint256 consultId) public onlyStudent consultExists(consultId) {
+    /**
+     * @notice For a student to join a consultation session
+     * @param consultId The consultID of the consulation the student wants to join
+     */
+    function signUpForConsult(uint256 consultId) external onlyStudent consultExists(consultId) {
         Consult storage _consult = consults[consultId];
         require(_consult.studentsSignedUp.length < _consult.capacity, "Consult is already at maximum capacity");
         require(balanceOf(msg.sender) >= _consult.stake, "Student has insufficient balance");
+        Student storage _student = students[msg.sender];
+        require(_student.consults[consultId] == ConsultAttendance.NOT_SIGNED_UP, "Student has already signed up");
         _transfer(msg.sender, address(this), _consult.stake);
         _consult.studentsSignedUp.push(msg.sender);
-        Student storage _student = students[msg.sender];
-        _student.consults[consultId].signedUp = true;
+        _student.consults[consultId] = ConsultAttendance.SIGNED_UP;
     }
 
-    function markConsultAttendance(uint256 consultId) public onlyTeacher consultExists(consultId) {}
+    /**
+     * @notice For the teacher that created the consultation session to mark attendance of students that attended
+     * @param consultId The consultId of the consulation
+     * @param studentAddr The wallet address of the student who's attendance is to be marked
+     */
+    function markConsultAttendance(
+        uint256 consultId,
+        address studentAddr
+    ) external onlyTeacher consultExists(consultId) {
+        Student storage _student = students[studentAddr];
+        require(_student.consults[consultId] == ConsultAttendance.SIGNED_UP, "Student did not sign up for consult");
+        Consult storage _consult = consults[consultId];
+        require(_consult.teacher == msg.sender, "Only the teacher hosting the consult can mark attendance");
+        require(balanceOf(address(this)) >= _consult.stake, "Treasury has insufficient balance");
+        _transfer(address(this), studentAddr, _consult.stake);
+        _student.consults[consultId] = ConsultAttendance.ATTENDED;
+    }
+
+    /**
+     * @notice For a teacher to create an assignment
+     * @param bounty The amount of DOJO tokens to be paid out to the student on completion of the assignment
+     * @param description A 1-liner description of the assignment
+     */
+    function createAssignment(
+        uint8 bounty,
+        string calldata description
+    ) external onlyTeacher notEmptyString(description) {
+        uint256 assignmentId = nextAssignmentId;
+        assignments[assignmentId] = Assignment(
+            assignmentId,
+            description,
+            msg.sender,
+            bounty,
+            address(0),
+            AssignmentApprovalState.PENDING_APPROVAL
+        );
+        nextAssignmentId++;
+        emit AssignmentCreated(assignmentId, msg.sender, bounty);
+    }
+
+    /**
+     * @notice For the principal to approve an assignment
+     * @param assignmentId The assignmentID of the assignment to be approved
+     */
+    function approveAssignment(uint256 assignmentId) external onlyPrincipal assignmentExists(assignmentId) {
+        Assignment storage _assignment = assignments[assignmentId];
+        require(balanceOf(address(this)) >= _assignment.bounty, "Treasury has insufficient balance");
+        _assignment.state = AssignmentApprovalState.APPROVED;
+        _approve(address(this), _assignment.teacher, _assignment.bounty);
+    }
+
+    /**
+     * @notice For a student to sign up for an approved assignment
+     * @param assignmentId The assignmentId of the assignment to be signed up for
+     */
+    function signUpForAssignment(uint256 assignmentId) external onlyStudent assignmentExists(assignmentId) {
+        Assignment storage _assignment = assignments[assignmentId];
+        require(_assignment.state == AssignmentApprovalState.APPROVED, "Assignment not approved yet");
+        require(_assignment.participant == address(0), "Assignment already has a participant");
+        _assignment.participant = msg.sender;
+        Student storage _student = students[msg.sender];
+        _student.assignments[assignmentId] = AssignmentParticipantState.SIGNED_UP;
+    }
+
+    /**
+     * @notice For a teacher to pay up the bounty of an assignment
+     * @param assignmentId The assignmentId of the assignment to pay out
+     */
+    function payOutAssignment(uint256 assignmentId) external onlyTeacher assignmentExists(assignmentId) {
+        Assignment storage _assignment = assignments[assignmentId];
+        require(_assignment.state == AssignmentApprovalState.APPROVED, "Assignment not approved yet");
+        require(_assignment.participant != address(0), "Assignment does not have a participant yet");
+        require(_assignment.teacher == msg.sender, "Only the teacher that created the assignment can pay it out");
+        _assignment.state = AssignmentApprovalState.COMPLETED;
+        transferFrom(address(this), _assignment.participant, _assignment.bounty);
+        Student storage _student = students[_assignment.participant];
+        _student.assignments[assignmentId] = AssignmentParticipantState.PAID_OUT;
+    }
 
     function _isEmptyString(string memory str_) internal pure returns (bool) {
         bytes memory bytesStr = bytes(str_);
@@ -177,14 +313,20 @@ contract Dojo is AccessControl, ERC20 {
     }
 
     modifier classExists(uint256 classId) {
-        Class memory class = classes[classId];
-        require(!_isEmptyString(class.name), "Class does not exist");
+        Class memory _class = classes[classId];
+        require(!_isEmptyString(_class.name), "Class does not exist");
         _;
     }
 
     modifier consultExists(uint256 consultId) {
-        Consult memory consult = consults[consultId];
-        require(consult.teacher != address(0), "Consult does not exist");
+        Consult memory _consult = consults[consultId];
+        require(_consult.teacher != address(0), "Consult does not exist");
+        _;
+    }
+
+    modifier assignmentExists(uint256 assignmentId) {
+        Assignment memory _assignment = assignments[assignmentId];
+        require(_assignment.teacher != address(0), "Assignment does not exist");
         _;
     }
 
